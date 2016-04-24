8a3ad0c58f4cbaf3015be21d2bc3da0bb4f0988e
Jiang Boyu <jbyjiangboyu@126.com>
2016-04-24 10:40:59 +0800

    Update BooksTableSeeder

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -17,7 +17,25 @@ class BooksTableSeeder extends Seeder
             'title' => 'The Birth of Tragedy',
             'author' => 'Friedrich Nietzsche',
             'published' => '1872',
-            'text' => 'Whatever might have been the basis for this dubious book, it must have been a question of the utmost importance and charm, as well as a deeply personal one at the time—testimony to that effect is the period in which it arose, in spite of which it arose, that disturbing era of the Franco-Prussian war of 1870-71.'
+            'text' => 'Whatever might have been the basis for this dubious book...'
+        ]);
+
+        DB::table('books')->insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'title' => 'Beyond Good and Evil',
+            'author' => 'Friedrich Nietzsche',
+            'published' => '1886',
+            'text' => 'Supposing trust to be a woman, what is the suspicion...'
+        ]);
+
+        DB::table('books')->insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'title' => 'All Quiet on the Western Front',
+            'author' => 'Erich Maria Remarque',
+            'published' => '1929',
+            'text' => 'We are at reat five miles behind the front...'
         ]);
     }
 }
